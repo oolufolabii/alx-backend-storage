b7bcd311826df01afc126b9820df57138eb17030
oolufolabii <otitoola.olufolabi@gmail.com>
2023-02-09 11:10:57 +0100

    task 101

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -10,9 +10,9 @@ BEGIN
         JOIN corrections as C ON U.id=C.user_id 
         JOIN projects AS P ON C.project_id=P.id 
         GROUP BY U.id)
-    AS WA
-    SET U.average_score = WA.w_avg 
-    WHERE U.id=WA.id;
+    AS W_A
+    SET U.average_score = W_A.w_avg 
+    WHERE U.id=W_A.id;
 END
 $$
 DELIMITER ;
