60e93b9841e9a012bcfe5648287d8429e4ffd4a7
oolufolabii <otitoola.olufolabi@gmail.com>
2023-02-09 09:47:01 +0100

    Task 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,5 @@
--- SQL Script Lists all bands with Glam rock as their main style, ranked by their longevity.
+-- SQL Script Lists all bands with Glam rock as their
+-- main style, ranked by their longevity.
 
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
     FROM metal_bands
